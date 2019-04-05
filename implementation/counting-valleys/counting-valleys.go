d48542ec9ed12886761b0c026314ced0d0ae28f5
Kevin Mayo <kevin.mayo@udacity.com>
2019-04-04 18:33:19 -0600

    add counting-valleys solution

--- implementation/counting-valleys/counting-valleys.go
+++ implementation/counting-valleys/counting-valleys.go
@@ -1,54 +1,70 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "os"
-    "strconv"
-    "strings"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // Complete the countingValleys function below.
 func countingValleys(n int32, s string) int32 {
-
-
+	valleys, elevation := 0, 0
+	inValley := false
+	for _, char := range s {
+		if char == 'U' {
+			if elevation == -1 && inValley {
+				inValley = false
+				valleys++
+			}
+			elevation++
+		}
+		if char == 'D' {
+			if elevation == 0 && !inValley {
+				inValley = true
+			}
+			elevation--
+		}
+	}
+	return int32(valleys)
 }
 
 func main() {
-    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
-    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-    checkError(err)
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
 
-    defer stdout.Close()
+	defer stdout.Close()
 
-    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
 
-    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
-    checkError(err)
-    n := int32(nTemp)
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
 
-    s := readLine(reader)
+	s := readLine(reader)
 
-    result := countingValleys(n, s)
+	result := countingValleys(n, s)
 
-    fmt.Fprintf(writer, "%d\n", result)
+	fmt.Fprintf(writer, "%d\n", result)
 
-    writer.Flush()
+	writer.Flush()
 }
 
 func readLine(reader *bufio.Reader) string {
-    str, _, err := reader.ReadLine()
-    if err == io.EOF {
-        return ""
-    }
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
 
-    return strings.TrimRight(string(str), "\r\n")
+	return strings.TrimRight(string(str), "\r\n")
 }
 
 func checkError(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
