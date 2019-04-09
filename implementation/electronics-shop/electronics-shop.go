c9e21471bd1a742483a894155d98cc0b62a00717
Kevin Mayo <kevin.mayo@udacity.com>
2019-04-09 11:47:31 -0600

    add electronics-shop answer

--- implementation/electronics-shop/electronics-shop.go
+++ implementation/electronics-shop/electronics-shop.go
@@ -1,92 +1,112 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "os"
-    "strconv"
-    "strings"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 /*
  * Complete the getMoneySpent function below.
  */
 func getMoneySpent(keyboards []int32, drives []int32, b int32) int32 {
-    /*
-     * Write your code here.
-     */
-
+	var keyboard, current, highest int32
+	sort.Slice(keyboards, func(i, j int) bool { return keyboards[j] < keyboards[i] })
+	sort.Slice(drives, func(i, j int) bool { return drives[j] < drives[i] })
+	fmt.Printf("%v | %v", keyboards, drives)
+	for k := 0; k < len(keyboards); k++ {
+		if keyboards[k] < b {
+			keyboard = keyboards[k]
+			for d := 0; d < len(drives); d++ {
+				current = keyboard + drives[d]
+				if current == b {
+					return b
+				}
+				if current < b && current > highest {
+					highest = current
+				}
+			}
+		}
+	}
+	if highest != 0 {
+		return highest
+	} else {
+		return int32(-1)
+	}
 }
 
 func main() {
-    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
-    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-    checkError(err)
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
 
-    defer stdout.Close()
+	defer stdout.Close()
 
-    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
 
-    bnm := strings.Split(readLine(reader), " ")
+	bnm := strings.Split(readLine(reader), " ")
 
-    bTemp, err := strconv.ParseInt(bnm[0], 10, 64)
-    checkError(err)
-    b := int32(bTemp)
+	bTemp, err := strconv.ParseInt(bnm[0], 10, 64)
+	checkError(err)
+	b := int32(bTemp)
 
-    nTemp, err := strconv.ParseInt(bnm[1], 10, 64)
-    checkError(err)
-    n := int32(nTemp)
+	nTemp, err := strconv.ParseInt(bnm[1], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
 
-    mTemp, err := strconv.ParseInt(bnm[2], 10, 64)
-    checkError(err)
-    m := int32(mTemp)
+	mTemp, err := strconv.ParseInt(bnm[2], 10, 64)
+	checkError(err)
+	m := int32(mTemp)
 
-    keyboardsTemp := strings.Split(readLine(reader), " ")
+	keyboardsTemp := strings.Split(readLine(reader), " ")
 
-    var keyboards []int32
+	var keyboards []int32
 
-    for keyboardsItr := 0; keyboardsItr < int(n); keyboardsItr++ {
-        keyboardsItemTemp, err := strconv.ParseInt(keyboardsTemp[keyboardsItr], 10, 64)
-        checkError(err)
-        keyboardsItem := int32(keyboardsItemTemp)
-        keyboards = append(keyboards, keyboardsItem)
-    }
+	for keyboardsItr := 0; keyboardsItr < int(n); keyboardsItr++ {
+		keyboardsItemTemp, err := strconv.ParseInt(keyboardsTemp[keyboardsItr], 10, 64)
+		checkError(err)
+		keyboardsItem := int32(keyboardsItemTemp)
+		keyboards = append(keyboards, keyboardsItem)
+	}
 
-    drivesTemp := strings.Split(readLine(reader), " ")
+	drivesTemp := strings.Split(readLine(reader), " ")
 
-    var drives []int32
+	var drives []int32
 
-    for drivesItr := 0; drivesItr < int(m); drivesItr++ {
-        drivesItemTemp, err := strconv.ParseInt(drivesTemp[drivesItr], 10, 64)
-        checkError(err)
-        drivesItem := int32(drivesItemTemp)
-        drives = append(drives, drivesItem)
-    }
+	for drivesItr := 0; drivesItr < int(m); drivesItr++ {
+		drivesItemTemp, err := strconv.ParseInt(drivesTemp[drivesItr], 10, 64)
+		checkError(err)
+		drivesItem := int32(drivesItemTemp)
+		drives = append(drives, drivesItem)
+	}
 
-    /*
-     * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
-     */
+	/*
+	 * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+	 */
 
-    moneySpent := getMoneySpent(keyboards, drives, b)
+	moneySpent := getMoneySpent(keyboards, drives, b)
 
-    fmt.Fprintf(writer, "%d\n", moneySpent)
+	fmt.Fprintf(writer, "%d\n", moneySpent)
 
-    writer.Flush()
+	writer.Flush()
 }
 
 func readLine(reader *bufio.Reader) string {
-    str, _, err := reader.ReadLine()
-    if err == io.EOF {
-        return ""
-    }
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
 
-    return strings.TrimRight(string(str), "\r\n")
+	return strings.TrimRight(string(str), "\r\n")
 }
 
 func checkError(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
