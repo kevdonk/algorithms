c1c8ba7662a15c1eda40e880d8e7331173d713ba
Kevin Mayo <kevin.mayo@udacity.com>
2019-05-21 10:33:13 -0600

    add new-year-chaos solution

--- arrays/new-year-chaos/new-year-chaos.go
+++ arrays/new-year-chaos/new-year-chaos.go
@@ -9,10 +9,31 @@ import (
     "strings"
 )
 
+func max(a, b int32) int32 {
+    if a > b {
+        return a
+    }
+    return b
+}
 // Complete the minimumBribes function below.
 func minimumBribes(q []int32) {
-
-
+    bribes := int32(0)
+    for i, x := range q {
+        // no one can move more than 2 spots forwards
+        if (x - (int32(i)+1)) > int32(2) {
+            fmt.Println("Too chaotic")
+            return
+        }
+        // count how many bribes each person has recieved
+        // max 2 bribes, so bribers of i can only get to x-1 (-1 for index)
+        start := max(x-int32(2),0)
+        for j := start; j < int32(i); j++ {
+            if q[j] > x {
+                bribes++
+            }
+        }
+    }
+    fmt.Println(bribes)
 }
 
 func main() {
