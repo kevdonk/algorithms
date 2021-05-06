039f999ca41ea3d4a3ce6b2de30f221d80025ee7
Kevin Mayo <kevin.mayo@udacity.com>
2021-05-06 10:47:46 -0600

    solve encrytpion

--- implementation/encryption/encryption.go
+++ implementation/encryption/encryption.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"os"
-	"strings"
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strings"
+    "math"
 )
 
 /*
@@ -14,42 +15,72 @@ import (
  * The function is expected to return a STRING.
  * The function accepts STRING s as parameter.
  */
+// 0 1 2 3
+// f e e d
+// 4 5 6 7
+// t h e d
+// 8 9 10 11
+// o g
+
+// 0 4 8
+// 1 5 9
+// 2 6 (10)
+// 3 7 (11)
+
+// rows 3
+// columns 4
+// 0 1 2 3 4 5 6 7 8 9 10
 
 func encryption(s string) string {
-	// Write your code here
-
+    length := float64(len(s))
+    root := math.Sqrt(length)
+    rows := math.Floor(root)
+    columns := math.Ceil(root)
+    if rows * columns < length {
+        rows++
+    }
+    var stripped string
+    stripped = strings.ReplaceAll(s, " ", "")
+    var encrypted string
+    for y := float64(0); y < columns; y++ {
+        for x := float64(0); x + y < length; x+=columns {
+            encrypted = encrypted + string(stripped[int(x+y)])
+        }
+        encrypted = encrypted + " "
+    }
+    return encrypted
 }
 
 func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
 
-	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	checkError(err)
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
 
-	defer stdout.Close()
+    defer stdout.Close()
 
-	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
 
-	s := readLine(reader)
+    s := readLine(reader)
 
-	result := encryption(s)
+    result := encryption(s)
 
-	fmt.Fprintf(writer, "%s\n", result)
+    fmt.Fprintf(writer, "%s\n", result)
 
-	writer.Flush()
+    writer.Flush()
 }
 
 func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
 
-	return strings.TrimRight(string(str), "\r\n")
+    return strings.TrimRight(string(str), "\r\n")
 }
 
 func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 }
